70a4cb6cc1d73d5d85ab4a21aea35e11962679f4
jonasRamosz <jrenovaveis@gmail.com>
2023-04-10 21:58:02 -0300

    projeto do biscoito da sorte

---
+++ src/components/botao/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Botao extends Component{
+    render(){
+        return(
+            <div>
+                <button onClick={this.props.acaoBtn}> {this.props.nome} </button>
+            </div>
+        );
+    }
+}
+export default Botao;
